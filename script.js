faf30aeb87494ebed15a385b7a145880abf7d8b4
JeffenFay <jeffen.fay@gmail.com>
2018-11-09 09:33:15 +0100

    Done !

--- script.js
+++ script.js
@@ -8,10 +8,21 @@ ctx.arc(205, 110, 50, 0, 2*Math.PI, true);
 ctx.fillStyle = '#976F0F';
 ctx.fill();
 ctx.beginPath();
-ctx.moveTo(75, 75);
-ctx.bezierCurveTo(85, 140, 125, 140, 150, 75);
-ctx.bezierCurveTo(100, 140, 100, 140, 150, 75);
+ctx.moveTo(180, 130);
+ctx.bezierCurveTo(190, 145, 220, 145, 230, 130); // Bouche bas
+ctx.moveTo(180, 130);
+ctx.bezierCurveTo(190, 135, 220, 135, 230, 130); // Bouche bas
+ctx.strokeStyle = 'red';
 ctx.stroke();
+//Sourcils
+ctx.beginPath();
+ctx.moveTo(180, 88);
+ctx.bezierCurveTo(180, 80, 200, 80, 200, 88); // Sourcil gauche
+ctx.moveTo(210, 88);
+ctx.bezierCurveTo(210, 80, 230, 80, 230, 88); // Sourcil gauche
+ctx.strokeStyle = 'white';
+ctx.stroke();
+// Yeux
 ctx.beginPath();
 ctx.moveTo(65, 65);
 ctx.arc(190, 100, 8, 0, Math.PI * 2, true);  // Oeil gauche
@@ -35,10 +46,16 @@ ctx.quadraticCurveTo(175, 380, 160, 350);// corps + côté jambe droite
 ctx.quadraticCurveTo(160, 190, 160, 190);// aisselle gauche
 ctx.quadraticCurveTo(60, 190, 60, 190);// bas bras gauche
 ctx.quadraticCurveTo(20, 170, 60, 150);// haut bras gauche
-// ctx.quadraticCurveTo(5, 395, 220, 100);
 ctx.fillStyle = '#976F0F';
 ctx.fill();
-
+// Boutons
+ctx.beginPath();
+ctx.moveTo(205, 210);
+ctx.arc(205, 210, 8, 0, Math.PI * 2, true);  // Bouton du haut
+ctx.moveTo(205, 240);
+ctx.arc(205, 240, 8, 0, Math.PI * 2, true);  // Bouton du bas
+ctx.fillStyle = '#7A2976';
+ctx.fill();
 // soucoupe
 //bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y) cp=> poignées
 //dôme
